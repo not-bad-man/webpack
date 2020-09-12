70edcf2913c4abefbb85c48f29304370d70fc5af
not-bad-man <1926529213@qq.com>
2020-09-12 15:17:09 +0800

    初始化

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,14 +1,47 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const rootPath = path.join(__dirname, '../');
-console.log('rootpath', rootPath)
 
 module.exports = {
   entry: {
-		path: path.join(rootPath, './src/index.js')
+		index: path.join(rootPath, './src/index.js')
 	},
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, '../output')
+		path: path.resolve(__dirname, '../output'),
+		filename: 'static/js/[name][hash:8].js',
+		chunkFilename: 'static/js/[name][hash:8].js'
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: 'template/index.html',
+			filename: 'tpl/index.html'
+		}),
+		new CleanWebpackPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js|jsx|ts|tsx$/,
+				use: 'babel-loader',
+				exclude: '/node_modules/'
+			},
+			{
+				test: /\.css|less$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'less-loader'
+				]
+			}
+		]
+	},
+	resolve: {
+		extensoins: ['js', 'ts', 'jsx', 'tsx']
+	},
+	devServer: {
+		port: 3000,
+		hot: true
 	}
 }
